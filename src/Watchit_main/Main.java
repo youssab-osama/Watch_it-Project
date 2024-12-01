7b2292fb732236a6c435cb4b595c1e0fd2b57b4f
youssab-osama <Youssab235@gmail.com>
2024-12-01 19:03:39 +0200

    new commit changes

--- src/Watchit_main/Main.java
+++ src/Watchit_main/Main.java
@@ -2,6 +2,6 @@ package Watchit_main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Commit to other branch");
+        System.out.println("Commit to other branch;");
     }
 }
